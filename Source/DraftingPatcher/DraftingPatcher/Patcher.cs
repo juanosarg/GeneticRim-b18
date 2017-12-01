c104ebe41b338616745eb9868b2c5cd384d72c90
juanosarg <31105173+juanosarg@users.noreply.github.com>
2017-12-01 16:47:44 +0100

    v2.4

--- Source/DraftingPatcher/DraftingPatcher/Patcher.cs
+++ Source/DraftingPatcher/DraftingPatcher/Patcher.cs
@@ -1,26 +1,26 @@
 ﻿using Harmony;
 using RimWorld;
+using System.Reflection;
+using Verse;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Reflection;
-using System.Text;
-using UnityEngine;
-using Verse;
 using Verse.AI;
-using System.Diagnostics;
-using Verse.AI.Group;
+
+
 
 namespace DraftingPatcher
 {
     [StaticConstructorOnStartup]
-    class Main
+    public class Main
     {
         static Main()
         {
             var harmony = HarmonyInstance.Create("com.geneticrim");
             harmony.PatchAll(Assembly.GetExecutingAssembly());
         }
+
+     
     }
     [HarmonyPatch(typeof(PawnComponentsUtility))]
     [HarmonyPatch("AddAndRemoveDynamicComponents")]
@@ -41,6 +41,108 @@ namespace DraftingPatcher
     }
 
    
+
+    [HarmonyPatch(typeof(Pawn))]
+    [HarmonyPatch("GetGizmos")]
+
+    static class Pawn_GetGizmos_Patch
+    {
+        [HarmonyPostfix]
+      
+        public static void AddGizmo(Pawn __instance, ref IEnumerable<Gizmo> __result)
+        {
+            var pawn = __instance;
+            var gizmos = __result.ToList();
+            bool flag = pawn.Faction != null && pawn.Faction.IsPlayer;
+
+            if ((pawn.drafter != null) && (pawn.kindDef.ToString() == "GR_Bearodile") && flag)
+            {
+                Command_Action mygizmo = new Command_Action();
+                mygizmo.action = delegate
+                {
+                    pawn.drafter.Drafted= !pawn.drafter.Drafted;
+                };
+                mygizmo.defaultDesc = "CommandToggleDraftDesc".Translate();
+                mygizmo.icon = TexCommand.Draft;
+                gizmos.Insert(0, mygizmo);
+            }
+
+            if ((pawn.drafter != null) && (pawn.kindDef.ToString() == "GR_Bearodile") && flag && pawn.drafter.Drafted)
+            {
+
+                Command_Target command_Target = new Command_Target();
+                command_Target.defaultLabel = "CommandMeleeAttack".Translate();
+                command_Target.defaultDesc = "CommandMeleeAttackDesc".Translate();
+                command_Target.targetingParams = TargetingParameters.ForAttackAny();
+                command_Target.icon = TexCommand.AttackMelee;
+               
+                command_Target.action = delegate (Thing target)
+                {
+                    IEnumerable<Pawn> enumerable = Find.Selector.SelectedObjects.Where(delegate (object x)
+                    {
+                        Pawn pawn2 = x as Pawn;
+                        return pawn2 != null &&  pawn2.Drafted;
+                    }).Cast<Pawn>();
+                    foreach (Pawn current in enumerable)
+                    {
+                        Job job = new Job(JobDefOf.AttackMelee, target);
+                        Pawn pawn2 = target as Pawn;
+                        if (pawn2 != null)
+                        {
+                            job.killIncappedTarget = pawn2.Downed;
+                        }
+                        pawn.jobs.TryTakeOrderedJob(job, JobTag.Misc);
+                    }
+                };
+                gizmos.Insert(1, command_Target);
+
+
+
+
+
+            }
+
+            __result = gizmos;
+        }
+    }
+
+    [HarmonyPatch(typeof(FloatMenuMakerMap))]
+    [HarmonyPatch("CanTakeOrder")]
+    public static class FloatMenuMakerMap_CanTakeOrder_Patch
+    {
+        [HarmonyPostfix]
+        public static void MakePawnControllable(Pawn pawn, ref bool __result)
+
+        {
+            bool flag = pawn.Faction != null && pawn.Faction.IsPlayer;
+            if ((pawn.kindDef.ToString() == "GR_Bearodile") && flag)
+            {
+                //Log.Message("You should be controllable now");
+                __result = true;
+            }
+            
+        }
+    }
+
+    [HarmonyPatch(typeof(FloatMenuMakerMap))]
+    [HarmonyPatch("TryMakeFloatMenu")]
+    public static class FloatMenuMakerMap_TryMakeFloatMenu_Patch
+    {
+        [HarmonyPrefix]
+        public static void CheckControllable(Pawn pawn)
+
+        {
+            bool flag = pawn.Faction != null && pawn.Faction.IsPlayer;
+            if ((pawn.kindDef.ToString() == "GR_Bearodile") && flag)
+            {
+                return;
+            }
+
+        }
+    }
+
+
+
 
 
 }
